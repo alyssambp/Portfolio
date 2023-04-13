2a481c26555dda1b4240db62aef994e78e7983ad
Alyssa Pagulayan <99alyssapagulayan@gmail.com>
2023-04-13 14:17:16 +0100

    Form validation - email

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,4 @@
+// Responsive Navigation - Hamburger
 const hamburger = document.querySelector(".hamburger");
 const navMenu = document.querySelector(".nav-menu");
 
@@ -10,3 +11,115 @@ document.querySelectorAll(".nav-link").forEach(n => n.addEventListener("click", 
   hamburger.classList.remove("active");
   navMenu.classList.remove("active");
 }))
+
+
+
+// Form Validation
+const emailEl = document.querySelector('#email');
+const form = document.querySelector('#contact-form');
+
+// check email field
+const checkEmail = () => {
+
+  let valid = false;
+  const email = emailEl.value.trim();
+  if (!isRequired(email)) {
+    showError(emailEl, 'Email cannot be blank.')
+  } else if (!isEmailValid(email)){
+    showError(emailEl, 'Email is not valid.')
+  } else {
+    showSuccess(emailEl);
+    valid = true;
+  }
+
+  return valid;
+};
+
+const isEmailValid = (email) => {
+  const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return re.test(email);
+};
+
+const isRequired = value => value === '' ? false : true;
+
+const showError = (input, message) => {
+  // get the form-field element
+  const formField = input.parentElement;
+  // add the error class
+  formField.classList.remove('success');
+  formField.classList.add('error');
+
+  // show the error message
+  const error = formField.querySelector('small');
+  error.textContent = message;
+};
+
+const showSuccess = (input) => {
+  // get the form-field element
+  const formField = input.parentElement;
+
+  // remove the error class
+  formField.classList.remove('error');
+  formField.classList.add('success');
+
+  // hide the error message
+  const error = formField.querySelector('small');
+  error.textContent = '';
+}
+
+form.addEventListener('submit', function (e) {
+  //prevent the form from submitting
+  e.preventDefault();
+
+  //validate fields
+  let isEmailValid = checkEmail();
+
+  let isFormValid = isEmailValid;
+
+  // submit to the server if the form is valid
+  if (isFormValid) {
+
+  }
+
+});
+
+form.addEventListener('submit', function (e) {
+    // prevent the form from submitting
+    e.preventDefault();
+
+    // validate fields
+    let isEmailValid = checkEmail();
+
+    let isFormValid = isEmailValid;
+
+    // submit to the server if the form is valid
+    if (isFormValid) {
+
+    }
+});
+
+const debounce = (fn, delay = 500) => {
+    let timeoutId;
+    return (...args) => {
+        // cancel the previous timer
+        if (timeoutId) {
+            clearTimeout(timeoutId);
+        }
+        // setup a new timer
+        timeoutId = setTimeout(() => {
+            fn.apply(null, args)
+        }, delay);
+    };
+};
+
+form.addEventListener('input', debounce(function (e) {
+  switch (e.target.id) {
+      case 'email':
+          checkEmail();
+          break;
+  }
+}));
+
+
+
+
